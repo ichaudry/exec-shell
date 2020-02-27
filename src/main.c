cf7c22edaaf9fc11622e8507bab41a56baf37138
ichaudry <ibraheem27496@gmail.com>
2020-02-26 22:01:24 -0800

    Simplified the get line function

--- src/main.c
+++ src/main.c
@@ -13,21 +13,14 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
-
 char *getInputLine();
 char ** getInputArguments(char * inputLine);
-
 
 int main(int argc, char *argv[]){
     int runStatus;
     char *prompt;
     char * inputLine;
     char ** inputArgs;
-
-
-
    
     //Takes the command line arguments to initalize prompt
     if(argc==2){
@@ -47,91 +40,41 @@ int main(int argc, char *argv[]){
         //Get input line from the user
         inputLine= getInputLine();
 
-
-        //Print input line as a test
-        // printf("%s \n",inputLine);
-
-
         //Break input line into arguments
         inputArgs= getInputArguments(inputLine);
 
-        // printf("\n %s     %s",inputArgs[1],inputArgs[3]);
-
-
-        
-
-
-        
+        printf("%s checking null %s",inputArgs[0],inputArgs[2]);
+   
     }
     while(runStatus);
-
-
 
     return 0; 
 }
 
 
-// char *getInputLine(void)
-// {
-//     // char *linePtr;
-//     // size_t buffer = 0; 
-
-//     // // //Segmenting buffer and catching errors with allocation
-//     // // linePtr = (char *)malloc(buffer * sizeof(char));
-//     // // if( linePtr == NULL)
-//     // // {
-//     // //     perror("Unable to allocate buffer");
-//     // //     exit(1);
-//     // // }
-
-//     // getline(&linePtr, &buffer, stdin);
-//     // return linePtr;
-// }
-
 char *getInputLine()
 {
-    int buffer= 1024;
-    char *linePtr= malloc(sizeof(char)* buffer);
-    char tempChar;
-    int stringIndex=0;
-    
+    char *linePtr;
+    size_t buffer = 1024; 
+
     //Segmenting buffer and catching errors with allocation
+    linePtr = (char *)malloc(buffer * sizeof(char));
     if( linePtr == NULL)
     {
         perror("Unable to allocate buffer");
         exit(1);
     }
 
-    //Loop gets characters until EOF or until the buufer size is reached 
-    while(1){
-        
-        //Read in a character from input 
-        tempChar= getchar();
-
-        //checks if end of file or end of line or buffer size is reached and returns resulting character array 
-        if (tempChar == EOF || tempChar == '\n' || stringIndex == buffer) 
-        {
-            linePtr[stringIndex] = '\0';
-            return linePtr;
-        } 
-        else 
-        {
-            linePtr[stringIndex] = tempChar;
-        }
-        stringIndex++;
-
-    }
+    getline(&linePtr, &buffer, stdin);
+    return linePtr;
 }
 
 
   char ** getInputArguments(char * inputLine){
-
-
     int bufferSize= 64;
     char **args = malloc(bufferSize * sizeof(char*));
     char * arg;
     int index=0;
-
 
     arg = strtok(inputLine, " \0\n");
     while (arg != NULL)
@@ -147,7 +90,6 @@ char *getInputLine()
             exit(EXIT_FAILURE);
         }
      }
-
         arg = strtok(NULL, " \0\n");
     }
 
@@ -155,3 +97,41 @@ char *getInputLine()
     return args;
 
     }
+
+
+
+    // char *getInputLine()
+// {
+//     int buffer= 1024;
+//     char *linePtr= malloc(sizeof(char)* buffer);
+//     char tempChar;
+//     int stringIndex=0;
+    
+//     //Segmenting buffer and catching errors with allocation
+//     if( linePtr == NULL)
+//     {
+//         perror("Unable to allocate buffer");
+//         exit(1);
+//     }
+
+//     //Loop gets characters until EOF or until the buufer size is reached 
+//     while(1){
+        
+//         //Read in a character from input 
+//         tempChar= getchar();
+
+//         //checks if end of file or end of line or buffer size is reached and returns resulting character array 
+//         if (tempChar == EOF || tempChar == '\n' || stringIndex == buffer) 
+//         {
+//             linePtr[stringIndex] = '\0';
+//             return linePtr;
+//         } 
+//         else 
+//         {
+//             linePtr[stringIndex] = tempChar;
+//         }
+//         stringIndex++;
+
+//     }
+// }
+
